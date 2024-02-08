f2995f953c1e3575f9041244f57c3173dad7b303
adamokennie <adamokennie@gmail.com>
2024-02-08 14:05:59 +0100

    logo and daisy ui

--- foodie-client/src/layout/Main.jsx
+++ foodie-client/src/layout/Main.jsx
@@ -1,9 +1,10 @@
+import React from 'react'
 import { Outlet } from "react-router-dom"
-
+import Navbar from "../components/Navbar"
 const Main = () => {
   return (
     <div>
-      <nav>nav bar</nav>
+      <Navbar/>
       <Outlet/>
       <footer>Footer</footer>
     </div>
